b0f01a25f37cf5d31cb15952a1b42129f035d39f
Drew Kitch <drew.kitch@gmail.com>
2019-01-18 14:01:27 -0500

    first commit

---
+++ fargate.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+# preinstall
+if [ -x "$(command -v apk)" ]; then
+	apk add --update curl
+fi
+
+# install
+echo
+echo 'Install Fargate'
+curl -L -o /usr/local/bin/fargate https://github.com/cloudposse/fargate/releases/download/0.1.0/fargate_linux_arm
+chmod +x /usr/local/bin/fargate
+
+echo
+echo 'Fargate Command'
+command -v fargate
+fargate --version
