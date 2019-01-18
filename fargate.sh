4cf6e41c3363a63fde5919438616c887e75770f1
Drew Kitch <drew.kitch@gmail.com>
2019-01-18 15:12:58 -0500

    change to 386

--- fargate.sh
+++ fargate.sh
@@ -9,7 +9,7 @@ fi
 # install
 echo
 echo 'Install Fargate'
-curl -L -o /usr/local/bin/fargate https://github.com/cloudposse/fargate/releases/download/0.1.0/fargate_linux_arm
+curl -L -o /usr/local/bin/fargate https://github.com/cloudposse/fargate/releases/download/0.1.0/fargate_linux_386
 chmod +x /usr/local/bin/fargate
 
 echo
